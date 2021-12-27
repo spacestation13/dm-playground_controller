b3040b287eebb7c76c1a83874c4c8c492b593bf6
ZeWaka <zewakagamer@gmail.com>
2021-12-27 09:36:33 -0600

    Signals are now numbers instead of full names

--- src/signal.rs
+++ src/signal.rs
@@ -1,11 +1,13 @@
 use std::str::FromStr;
 
 use nix::sys::signal;
+use phf::phf_map;
 
 /// Send the specified signal to the process with the specified pid.
 /// Returns Ok if the signal was sent, or an Err if the signal could not be sent.
 pub fn send_signal(pid: &&str, signal: &&str) -> Result<String, String> {
-    let sig = signal::Signal::from_str(signal).expect("Malformed signal number");
+    let sign = SIGNALS.get(signal).expect("Malformed signal number");
+    let sig = signal::Signal::from_str(sign).expect("Malformed signal name");
 
     match signal::kill(
         nix::unistd::Pid::from_raw(pid.parse::<i32>().expect("Malformed pid")),
@@ -18,3 +20,40 @@ pub fn send_signal(pid: &&str, signal: &&str) -> Result<String, String> {
         )),
     }
 }
+
+/// Generated from running `kill -l` on the image
+static SIGNALS: phf::Map<&'static str, &'static str> = phf_map! {
+    "1" => "SIGHUP",
+    "2" => "SIGINT",
+    "3" => "SIGQUIT",
+    "4" => "SIGILL",
+    "5" => "SIGTRAP",
+    "6" => "SIGABRT",
+    "7" => "SIGBUS",
+    "8" => "SIGFPE",
+    "9" => "SIGKILL",
+    "10" => "SIGUSR1",
+    "11" => "SIGSEGV",
+    "12" => "SIGUSR2",
+    "13" => "SIGPIPE",
+    "14" => "SIGALRM",
+    "15" => "SIGTERM",
+    "16" => "SIGSTKFLT",
+    "17" => "SIGCHLD",
+    "18" => "SIGCONT",
+    "19" => "SIGSTOP",
+    "20" => "SIGTSTP",
+    "21" => "SIGTTIN",
+    "22" => "SIGTTOU",
+    "23" => "SIGURG",
+    "24" => "SIGXCPU",
+    "25" => "SIGXFSZ",
+    "26" => "SIGVTALRM",
+    "27" => "SIGPROF",
+    "28" => "SIGWINCH",
+    "29" => "SIGPOLL",  // No entry in Signal
+    "30" => "SIGPWR",
+    "31" => "SIGSYS",
+    "34" => "SIGRTMIN", // No entry in Signal
+    "64" => "SIGRTMAX", // No entry in Signal
+};
