4fe659e844a05abd9de9cf65db9a65b424c271b1
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-12-30 14:41:28 -0500

    Fixes poll data not getting cleared when polling

--- src/poll.rs
+++ src/poll.rs
@@ -6,11 +6,12 @@ use std::sync::{Arc, Mutex};
 
 /// Sends the poll data through the serial connection
 pub fn send_poll_data(data: &Arc<Mutex<Vec<PollData>>>) -> Result<String, String> {
-    let poll_data = &*data.lock().unwrap();
-
-    Ok(poll_data
+    let poll_data = &mut *data.lock().unwrap();
+    let poll_data_str = poll_data
         .iter()
         .map(|dat| format!("{} {}\n", dat.typ.to_string(), &dat.data))
         .collect::<Vec<String>>()
-        .concat())
+        .concat();
+    poll_data.clear();
+    Ok(poll_data_str)
 }
