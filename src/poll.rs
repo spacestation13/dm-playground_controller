27232d81072981cee8e51d1506efea664981344c
ZeWaka <zewakagamer@gmail.com>
2024-06-25 00:58:52 -0700

    mega genius speedup for subprocesses
    i shrimply thought to myself "what if we just let it do its own thing and stop being such a fucking pest"
    
    it seems subprocesses agree

--- src/poll.rs
+++ src/poll.rs
@@ -7,6 +7,9 @@ use std::sync::{Arc, Mutex};
 /// Sends the poll data through the serial connection
 pub fn send_poll_data(data: &Arc<Mutex<Vec<PollData>>>) -> std::string::String {
     let poll_data = &mut *data.lock().unwrap();
+    if poll_data.is_empty() {
+        return String::new();
+    }
     let poll_data_str = poll_data
         .iter()
         .map(|dat| format!("{} {} {}\n", dat.typ, &dat.pid, &dat.data))
